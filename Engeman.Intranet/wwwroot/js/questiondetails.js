a8dc87205cbcf7ce782722f23412b35940e4c707
Michel Antunes <michelantunezqueiroz@gmail.com>
2022-08-18 16:44:13 -0300

    Implementação de uma guia para responder uma postagem do tipo pergunta na tela de detalhes da postagem

--- Engeman.Intranet/wwwroot/js/questiondetails.js
+++ Engeman.Intranet/wwwroot/js/questiondetails.js
@@ -1,4 +1,19 @@
 ﻿$(document).ready(function () {
+  $('.tags').tagsInput({
+    'height': 'auto',
+    'width': '100%',
+    'defaultText': '',
+  });
+
+  $("#tab_1_3").removeClass("active");
+})
+
+$("#answer-form").on("submit", function (event) {
+  //ignora o submit padrão do formulário
+  event.preventDefault();
+  if ($("#answer-form").valid()) {
+    console.log("ok");
+  }
 })
 
 $(".back-to-list-button").on("click", function () {
