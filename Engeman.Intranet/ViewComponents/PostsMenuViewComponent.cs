c941718e61803528ff2f9af8c4e8be7a5a4e2a13
Michel Antunes <michelantunezqueiroz@gmail.com>
2022-10-11 08:12:54 -0300

    Componentização: Menu de postagens e listagem de comentários

---
+++ Engeman.Intranet/ViewComponents/PostsMenuViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Engeman.Intranet.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+
+namespace Engeman.Intranet.ViewComponents
+{
+  public class PostsMenuViewComponent : ViewComponent
+  {
+    private readonly IUserAccountRepository _userAccount;
+    private readonly IPostRepository _postRepository;
+
+    public PostsMenuViewComponent(IUserAccountRepository userAccount, IPostRepository postRepository)
+    {
+      _userAccount = userAccount;
+      _postRepository = postRepository;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+      var userDomain = HttpContext.Session.GetString("_DomainUsername");
+      var user = _userAccount.GetUserAccountByDomainUsername (userDomain);
+      ViewBag.PostsNotRevisedCount = _postRepository.GetPostsByRestriction(user).AsQueryable().Where("revised == (@0)", false).Count();
+
+      return View(user);
+    }
+  }
+}
