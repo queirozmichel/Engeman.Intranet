1199273948bd48a517ab86273a2f59a2836f2c95
Michel Antunes <michelantunezqueiroz@gmail.com>
2022-12-21 10:58:49 -0300

    Migração do projeto para o .Net 6

--- Engeman.Intranet/Program.cs
+++ Engeman.Intranet/Program.cs
@@ -1,22 +1,58 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Engeman.Intranet.Library;
+using Engeman.Intranet.Repositories;
 
-namespace Engeman.Intranet {
-    public class Program {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+if (builder.Configuration.GetConnectionString("EngemanDb") != null)
+{
+    DatabaseInfo.ConnectionString = builder.Configuration.GetConnectionString("EngemanDb");
 }
+else
+{
+    throw new Exception("Unknown Connection String.");
+}
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication("CookieAuthentication").AddCookie("CookieAuthentication", options =>
+      {
+          options.Cookie.Name = "UserLoginCookie";
+          options.LoginPath = "/Login/Index";
+          options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+      });
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+      {
+          options.Cookie.Name = "UserSession";
+          options.IdleTimeout = TimeSpan.FromMinutes(20);
+          options.Cookie.HttpOnly = true;
+          options.Cookie.IsEssential = true;
+      });
+
+builder.Services.AddTransient<IUserAccountRepository, UserAccountRepository>();
+builder.Services.AddTransient<IPostRepository, PostRepository>();
+builder.Services.AddTransient<IDepartmentRepository, DepartmentRepository>();
+builder.Services.AddTransient<IPostFileRepository, PostFileRepository>();
+builder.Services.AddTransient<ICommentRepository, CommentRepository>();
+builder.Services.AddTransient<ICommentFileRepository, CommentFileRepository>();
+builder.Services.AddTransient<IPostRestrictionRepository, PostRestrictionRepository>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+app.MapControllerRoute(name: "default", pattern: "{controller=Login}/{action=Index}/{id?}");
+app.Run();
