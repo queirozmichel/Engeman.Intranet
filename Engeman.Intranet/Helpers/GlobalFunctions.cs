def8a71977b8c2e14474394cd4cb4e4ac2e4a031
Michel Antunes <michelantunezqueiroz@gmail.com>
2023-04-11 08:14:36 -0300

    Documentação adicionada na função 'NewLog'.

--- Engeman.Intranet/Helpers/GlobalFunctions.cs
+++ Engeman.Intranet/Helpers/GlobalFunctions.cs
@@ -3,7 +3,16 @@
 namespace Engeman.Intranet.Helpers
 {
   public class GlobalFunctions
-  {
+
+  {/// <summary>
+   /// Procedure para inserir um registro de log na tabela LOG.<br/>
+   /// Caso precise adicionar mais valores para 'operation' e 'registry_type' buscar pela procedure 'NewLog'
+   /// </summary>
+   /// <param name="operation">I = Inserção D = Exclusão U = Atualização A = Aprovação.</param>
+   /// <param name="registry_type">POS = Postagem COM = Comentário USU = Usuário.</param>
+   /// <param name="registry_id">ID do registro que foi inserido, modificado ou excluído.</param>
+   /// <param name="registry_table">Nome da tabela do registro que modificado, inserido ou excluído.</param>
+   /// <param name="username">Nome de usuário corrente que fez a modificação, inserção ou exclusão.</param>
     public static void NewLog(char operation, string registry_type, int registry_id, string registry_table, string username)
     {
       var query = $"EXEC NewLog '{operation}','{registry_type}',{registry_id},'{registry_table}','{username}'";
